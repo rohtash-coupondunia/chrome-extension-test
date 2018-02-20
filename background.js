8d8c62f135547e90fa49209c9d3ce1783d857663
CD <cd@CDFA-0133.local>
2018-02-20 16:16:42 +0530

    final draft

--- background.js
+++ background.js
@@ -3,6 +3,7 @@
 
 // hands on tab changing mechanism 
 
+/*
 chrome.tabs.onUpdated.addListener(function (tabid, changes,tab) {
     console.log("chnages occcuewd");
     console.log(changes)
@@ -11,3 +12,49 @@ chrome.tabs.onUpdated.addListener(function (tabid, changes,tab) {
       chrome.tabs.sendMessage(tab.id, {url: tab.url});
     }
 })
+*/
+
+
+
+//listen for msg
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  
+    if (request.action == "urlChanges") {
+
+      sendResponse({farewell: "goodbye"});
+
+      // save data to localStorge
+      saveData(request.url);
+      
+     }
+});
+
+
+
+function saveData (url) {
+  var urlArray = [];
+  chrome.storage.sync.get(["urlBucket"], function (data) {
+      if(data.urlBucket) {
+        urlArray = data.urlBucket;
+      }
+      console.log("stored data : ");
+      console.log(urlArray);
+
+      urlArray.push(url);
+      chrome.storage.sync.set({'urlBucket': urlArray}, function() {
+          console.log("stored in localStorge");
+      });
+
+  });
+}
+
+
+//icon click handler 
+chrome.browserAction.onClicked.addListener(function(tab) { 
+      console.log(tab);
+      chrome.storage.sync.get(["urlBucket"], function (data) {
+          console.log("retrive data");
+          chrome.tabs.sendMessage(tab.id, {action : 'iconClick', 'data': data});
+      });
+});
--- myscript.js
+++ myscript.js
@@ -1,4 +1,48 @@
  chrome.runtime.onMessage.addListener(
  function(request, sender) {
-   console.log(request.url);
+   console.log(request);
+   insertDiv(request);
 });
+
+
+
+
+ function init() {
+ 	console.log("inited");
+ 	let url = window.location.href;
+ 	chrome.runtime.sendMessage({action: "urlChanges", url : url}, function(response) {
+  		console.log(response.farewell);
+	});
+ }
+
+
+ window.onload = init;
+
+
+ function insertDiv(textData) {
+ 	document.documentElement.style.height = '100%';
+	document.body.style.height = '100%';
+	document.documentElement.style.width = '100%';
+	document.body.style.width = '100%';
+
+	var div = document.createElement( 'div' );
+	var btnForm = document.createElement( 'form' );
+	var btn = document.createElement( 'input' );
+
+	//append all elements
+	document.body.appendChild( div );
+	
+	//set attributes for div
+	div.id = 'myDivId';
+	div.style.position = 'fixed';
+	div.style.top = '10%';
+	div.style.left = '50%';
+	div.style.width = '500px';   
+	div.style.height = '150px';
+	div.style.backgroundColor = '#dcabab94';
+
+	var para  = document.createElement('p');
+	var node = document.createTextNode(JSON.stringify(textData));
+	para.appendChild(node);
+	div.appendChild(para);
+ }
